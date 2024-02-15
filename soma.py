d69c44e931630e133bc6f3e166be3d958d98265a
Kurry <kurry805@gmail.com>
2024-02-15 15:51:06 -0300

    soma

---
+++ soma.py
@@ -0,0 +1,7 @@
+print("olá tudo bem por favor insira dois números")
+
+num1=int(input("insira um número\n "))
+num2=int(input("insira um número \n"))
+
+print(f"a soma de {num1} + {num2} é igual a {num1+num2}")
+
