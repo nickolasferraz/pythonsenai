c987eaba6e41dcc2a4cd9e4ff3b1025f8399850d
Kurry <kurry805@gmail.com>
2024-02-16 15:02:29 -0300

    primeiros programas

---
+++ soma.py
@@ -0,0 +1,4 @@
+print("Boa tarde esse programa é para fazer somas \n")
+num1=int(input("digite um número \n"))
+num2=int(input("digite outro número\n"))
+print(f" O resultado da soma dos dois é {num1+num2}")
