0f66eee8620655424b441ef32a6a52035051c7fa
Kurry <kurry805@gmail.com>
2024-02-16 16:01:51 -0300

    calculadora

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+num1=10
+num2=10
+
+soma=num1+num2
+divisão=num1/num2
+multiplicao=num1*num2
+subtracao=num1-num2
+
+print(f"|Bem vindo a calculadora         |\n|soma de {num1} + {num2} é {soma}            |\n|A multiplicação de {num1} x {num2} é {multiplicao}|\n|A divisão de {num1} / {num2} é {divisão}      |\n|a subtração de {num1} - {num2} é {subtracao}      |")
--- soma.py
+++ soma.py
@@ -2,3 +2,6 @@ print("Boa tarde esse programa é para fazer somas \n")
 num1=int(input("digite um número \n"))
 num2=int(input("digite outro número\n"))
 print(f" O resultado da soma dos dois é {num1+num2}")
+
+print(type(num1))
+print(type(num2))
