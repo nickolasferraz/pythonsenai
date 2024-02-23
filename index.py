383351fd45f92b9f67cc776d391dea578d677486
kurry <kurry805@gmail.com>
2024-02-23 14:08:01 -0300

    dicas

--- index.py
+++ index.py
@@ -4,4 +4,9 @@
 
 tupla=(10,100,20,30,20)
 tupla[0]=22
-print(tupla)
+print(tupla[2])
+
+# o python não tem uma variavel const como no javascript então caso queria declara uma variavel imutavel é colocar o nome dele em maisculos
+# NOME=NICKOLAS
+# É UMA CONSTANTE
+# AGORA nome=Nickolas pode ser alterado 
